29a0ff8d815eb2f4ebc02b369923cbd4bf9817bd
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-06-29 22:47:56 +0200

    Initial Commit

---
+++ KanjiVGKit/KanjiVGKit.h
@@ -0,0 +1,19 @@
+//
+//  KanjiVGKit.h
+//  KanjiVGKit
+//
+//  Created by Daniel Schlaug on 6/29/14.
+//  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KanjiVGKit.
+FOUNDATION_EXPORT double KanjiVGKitVersionNumber;
+
+//! Project version string for KanjiVGKit.
+FOUNDATION_EXPORT const unsigned char KanjiVGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KanjiVGKit/PublicHeader.h>
+
+
